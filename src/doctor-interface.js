b7089711714900d615425fcd43dcfbd770be065f
Fatma <fceyda@gmail.com>
2020-02-14 11:34:03 -0800

    create basic html

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -2,3 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import {DoctorSevise} from './doctor-servise';
+
+$(document).ready(function() {
+
+
+  
+});
--- src/doctor-servise.js
+++ src/doctor-servise.js
@@ -1,4 +1,4 @@
-export class DoctorServise{
+export class DoctorServise {
   async searchDoctor(name, medicalIssue) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${medicalIssue}&location=45.512794%2C%20-122.679565%2C100&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=${process.env.API_KEY}
