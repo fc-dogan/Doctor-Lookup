2aa6f5fe5b01eaa621e4f43dd19a1ef12ff7bcab
Fatma <fceyda@gmail.com>
2020-02-14 20:38:37 -0800

    restructure ui logic and bussiness logic

--- src/doctor-servise.js
+++ src/doctor-servise.js
@@ -1,31 +1,31 @@
 export class DoctorServise {
-  async searchDoctorByName(name) {
+  async searchDoctorByName(name, issue) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.512230%2C-122.658722%2C100&sort=distance-asc&limit=25&user_key=${process.env.API_KEY}`);
-      
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=45.512230%2C-122.658722%2C100&sort=distance-asc&limit=25&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse;
       if(response.ok && response.status === 200) {
-       let jsonifiedResponse = await response.json();
-       return jsonifiedResponse;
+        jsonifiedResponse = await response.json();
       } else {
-        throw Error("There was an error processing your request")
+         jsonifiedResponse = false;
       }  
+      return jsonifiedResponse;
     }catch (error) {
       return false;
     }
   }
 
-  async searchDoctorByIssue(issue) {
-    try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+//   async searchDoctorByIssue(issue) {
+//     try {
+//       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       
-      if(response.ok && response.status === 200) {
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-       } else {
-         throw Error("There was an error processing your request")
-       }  
-     }catch (error) {
-       return false;
-     }
-    }
+//       if(response.ok && response.status === 200) {
+//         let jsonifiedResponse = await response.json();
+//         return jsonifiedResponse;
+//        } else {
+//          throw Error("There was an error processing your request")
+//        }  
+//      }catch (error) {
+//        return false;
+//      }
+//     }
 }
--- src/main.js
+++ src/main.js
@@ -11,42 +11,52 @@ $(document).ready(function() {
     event.preventDefault();
 
     let inputtedIssue = $('#health-issue').val();
-    let  inputtedDoctor = $('#doctor-name').val();
-    // $('#doctor-name').val("");
+    let inputtedDoctor = $('#doctor-name').val();
+    $('#doctor-name').val("");
+    $("#dr-form").hide();
+    $("#doctor-list").show();
 
+    let doctorServise = new DoctorServise();
     (async () => {
-      let doctorServise = new DoctorServise();
-      const response = await doctorServise.searchDoctorByName(inputtedDoctor);
+      
+      const response = await doctorServise.searchDoctorByName(inputtedDoctor, inputtedIssue);
       getElements(response);
       console.log(response);
     })();
 
      function getElements(response) { 
        if(response){
-         if( response.meta.count > 0 ) {
+         if( response.meta.count > 0 || doctorServise.inputtedIssue === null) {
           response.data.forEach(function(data) {
             $("#dr-list").append(`<tr><td>${data.profile.first_name} ${data.profile.last_name}</td><td>${data.profile.title}</td><td>${data.practices[0].visit_address.street}</td></tr>`)
           })
         } else {
-          $("#dr-list").text(`Sorry, there is no doctor meet your search criteria`)
+          $("#dr-list").append(`<tr><td>Sorry, there is no doctor meet your search criteria. <br><a href='index.html'> Click here</a> to try again</td></tr>`)
         }
-       }
+      } else {
+        $("#dr-list").append(`<tr><td>There was an error processing your request. <br><a href='index.html'> Click here</a> to try again</td></tr>`)
+      }
     }
 
-    (async () => {
-      let doctorServise = new DoctorServise();
-      const responseIssue = await doctorServise.searchDoctorByIssue(inputtedIssue);
-      getElementsByIssue(responseIssue);
-      console.log(responseIssue);
-    })();
+    // (async () => {
+    //   let doctorServise = new DoctorServise();
+    //   const responseIssue = await doctorServise.searchDoctorByIssue(inputtedIssue);
+    //   getElementsByIssue(responseIssue);
+    //   console.log(responseIssue);
+    // })();
 
-    function getElementsByIssue(responseIssue) {
-      if(responseIssue){  
-        responseIssue.data.forEach(function(data) {
-          $("#dr-list").append(`<tr><td>${data.profile.first_name} ${data.profile.last_name}</td><td>${data.profile.title}</td></tr>`)
-        })
-      } 
-    }
+    // function getElementsByIssue(responseIssue) {
+    //   if(responseIssue){  
+    //     if(responseIssue.meta.count > 0 ){
+    //       responseIssue.data.forEach(function(data) {
+    //         $("#dr-list").html(`<tr><td>${data.profile.first_name} ${data.profile.last_name}</td><td>${data.profile.title}</td></tr>`)
+    //       })
+    //     }
+    //   }else {
+    //       $("#dr-list").text(`Sorry, there is no doctor meet your search criteria. 
+    //       <a href='index.html'>Click here</a> to try again.`)
+    //     }  
+    // }
 
   });
 
